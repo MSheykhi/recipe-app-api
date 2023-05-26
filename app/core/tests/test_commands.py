0a20ed30d79140f50ef0cf3cfe0e78ed65dcc32f
Mahdi <Mahdi@smartvaniya.ir>
2023-05-26 14:48:24 +0330

    ADD createsuperuser test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -15,7 +15,7 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    def test_wait_for_db_ready(Self, patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
 
